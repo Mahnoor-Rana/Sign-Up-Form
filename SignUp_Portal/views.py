180f84af00058a500f8858f8a4896f0e37155e62
Mahnoor Rana <mahnoorrana2002@gmail.com>
2022-06-23 04:31:59 -0700

    updated

--- SignUp_Portal/views.py
+++ SignUp_Portal/views.py
@@ -2,12 +2,6 @@ from urllib import request
 from django.shortcuts import render
 from .forms import SignUpForm,UserCreationForm
 
-
-
-def index(request):
-    return render(request, 'index.html')
-
-
 def signup(request):
     form = SignUpForm(request.POST)
     if form.is_valid :
